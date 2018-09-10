f99037aac66be7db5c8a4db7c0bd04c1fd350a5f
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-10 16:57:56 -0700

    Wrapped chrome.tabs.query around with a Event Listener to check on tabs being updated. By using chrome.tabs.onUpdated listener, it will enable automatic closing of the tabs that matches the blacklisted URL(s). In this instance, for development purposes, facebook.com.

--- background.js
+++ background.js
@@ -2,15 +2,17 @@ var params = {
   "url": [
   ]
 };
-chrome.tabs.query(params, function(tabs) {
-  console.log("DistractMeNot Background Inspect Console");
-  for(let i = 0; i < tabs.length; i++) {
-    // console.log(tabs[i].url);
-    if(tabs[i].url.indexOf("facebook.com") > 0) {
-      console.log("FOUND A MATCH!: " + tabs[i].url);
-      chrome.tabs.remove(tabs[i].id, function() {
-        console.log("REMOVED: " + tabs[i].url);
-      });
+
+chrome.tabs.onUpdated.addListener(function() {
+  chrome.tabs.query(params, function(tabs) {
+    console.log("DistractMeNot Background Inspect Console");
+    for(let i = 0; i < tabs.length; i++) {
+      if(tabs[i].url.indexOf("facebook.com") > 0) {
+        console.log("FOUND A MATCH!: " + tabs[i].url);
+        chrome.tabs.remove(tabs[i].id, function() {
+          console.log("REMOVED: " + tabs[i].url);
+        });
+      }
     }
-  }
+  });
 });
