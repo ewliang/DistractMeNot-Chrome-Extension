bb38b20212be2ae4337a68f2b0b4942143670ced
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-10 22:03:16 -0700

    Added logic for receiving urlInput from user input on the client side via popup.html. Received and then pushed into a urlBlacklist array. Currently still testing with single URL hardcoded.

--- background.js
+++ background.js
@@ -1,18 +1,39 @@
 var params = {
-  "url": [
-  ]
+  "url": []
 };
 
+/*
+  Chrome Tabs Event Listener
+  - Constantly watch out for whenever tabs are updated (created or URL changed),
+    run the query to check if the blacklisted URLs are detected in the window.
+    If detected, close the window.
+*/
+var urlBlacklist = [];
 chrome.tabs.onUpdated.addListener(function() {
   chrome.tabs.query(params, function(tabs) {
     console.log("DistractMeNot Background Inspect Console");
     for(let i = 0; i < tabs.length; i++) {
-      if(tabs[i].url.indexOf("facebook.com") > 0) {
-        console.log("FOUND A MATCH!: " + tabs[i].url);
+      if(tabs[i].url.includes("https://myspace.com")) {
+        let matchedURL = tabs[i].url;
+        console.log("FOUND A MATCH!: " + matchedURL);
         chrome.tabs.remove(tabs[i].id, function() {
-          console.log("REMOVED: " + tabs[i].url);
+          console.log("REMOVED: " + matchedURL);
         });
       }
     }
   });
+});
+
+/*
+  Receive User Input from Popup.html
+  - In charge of listening for new URLs being added by user in frontend via
+  popup.html which gets sent to here. The URL is then added to a urlBlacklist array
+  which then gets looped through via the Chrome Tabs Event Listener to auto
+  close any tabs that are on the blacklist.
+*/
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // Update urlBlacklist with new URL.
+  urlBlacklist.push(request.url);
+  console.log(urlBlacklist);
+  sendResponse({ backendMessage: "Successfully Added: " + request.url });
 });
