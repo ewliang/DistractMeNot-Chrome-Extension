8be1fb663e97d12211f22bc10892049315515e45
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-12 08:50:09 -0700

    Added copyright.

---
+++ content.js
@@ -0,0 +1,7 @@
+// DistractMeNot - content.js
+// Author: Eric Liang
+// Author URI: https://www.eric-liang.com
+// Author Github URI: https://www.github.com/ewliang
+// Project Repository URI: https://github.com/ewliang/DistractMeNot-Chrome-Extension
+// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
+// License: MIT
--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
+// DistractMeNot - popup.js
+// Author: Eric Liang
+// Author URI: https://www.eric-liang.com
+// Author Github URI: https://www.github.com/ewliang
+// Project Repository URI: https://github.com/ewliang/DistractMeNot-Chrome-Extension
+// Description: Handles all the Chrome Extension UI popup.html activities (e.g. button clicks, etc.)
+// License: MIT
+
 var urlInput = document.getElementById('urlInput');
 var addURLButton = document.getElementById('addURLButton');
 var toggleActivation = document.getElementById('toggleActivation');
